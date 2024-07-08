5384825973030970a07c53aed81f09677c729e23
Abhishek Aman <abhiofficial.1599@gmail.com>
2024-07-09 02:15:54 +0530

    Leacture 17 - 20

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
+import Link from "next/link"
+
+export const metadata:Metadata = {
+    title: "Blog"
+}
+
 export default function Blog(){
-    return <h1> My Blogs </h1>;
+    return (
+    <>
+    <Link href="/"> Navigate to Home Page</Link>
+    <h1> My Blogs </h1>
+    </>);
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,24 @@
+import {Metadata} from "next";
+
+type Props = {
+    params:{
+        productId:string;
+    };
+};
+
+export const generateMetadata = async ({ params } : Props): Promise <Metadata> => {
+    
+    const title = await new Promise(resolve => 
+        setTimeout(()=>{
+            resolve(`Iphone ${params.productId}`)
+        },100)
+    );
+    
+    return {
+        title: `Product ${params.productId}`,
+    }
+}
+
 export default function ProductDetails({params}:{
     params : {productId : string}
 }){
